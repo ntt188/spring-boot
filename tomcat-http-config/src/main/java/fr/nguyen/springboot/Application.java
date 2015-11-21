16aba3bb493e747e04d16efae3168728bb8765ea
tungnguyen <ntt188@gmail.com>
2015-11-22 00:00:46 +0100

    Add properties files

--- tomcat-http-config/src/main/java/fr/nguyen/springboot/Application.java
+++ tomcat-http-config/src/main/java/fr/nguyen/springboot/Application.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by tungnguyen on 21/11/2015.
  */
 @SpringBootApplication
+@PropertySource("classpath:server.properties")
 public class Application {
 
     public static void main(String[] args) {
